e6ee79b04b537403157302cd9c8e5eeca538adca
Rob Saunders <rob@wearebrandnew.com>
2020-02-04 12:49:26 +0800

    added swift syntax dependency

--- Package.swift
+++ Package.swift
@@ -22,13 +22,14 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-syntax.git", .exact("0.50000.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "cdd-rpc-swift",
-            dependencies: []),
+            dependencies: ["SwiftSyntax"]),
         .testTarget(
             name: "cdd-rpc-swiftTests",
             dependencies: ["cdd-rpc-swift"]),
--- Sources/cdd-rpc-swift/Main.swift
+++ Sources/cdd-rpc-swift/Main.swift
@@ -1,1 +1,3 @@
+import SwiftSyntax
+
 print("hello")
